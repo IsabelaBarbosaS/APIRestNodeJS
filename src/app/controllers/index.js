817ba8635ae2cc33bb3438aec31114dbde41e980
Isabela <isabelabarbosado@gmail.com>
2022-09-28 16:14:05 -0300

    autorização de senha

---
+++ src/app/controllers/index.js
@@ -0,0 +1,10 @@
+//importa todos index de uma vez só para controller
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+    fs
+     .readdirSync(__dirname)
+     .filter(file => ((file.indexOf('.')) !== 0 && (file !== "index.js")))
+     .forEach(file => require(path.resolve(__dirname, file))(app));
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/noderest');
+mongoose.connect("mongodb://localhost:27017/apirestnode");
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
