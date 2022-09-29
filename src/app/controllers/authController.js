0505e64acd365400de87baa4ebbd9abd362c54f6
Isabela <isabelabarbosado@gmail.com>
2022-09-29 13:47:41 -0300

    reset password

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -89,12 +89,40 @@ router.post('/forgot_password', async (req, res) => {
             if (err)
                 return res.status(400).send({ error: 'Cannot send forgot password email' });
 
-         return res.send();
+            return res.send();
         })
 
     } catch (err) {
-        console.log(err);
         res.status(400).send({ error: 'Error on forgot password, try again' });
+    }
+});
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+            .select('+passwordResetToken passwordResetExpires');
+
+        if (!user)
+            return res.status(400).send({ error: 'User not found' });
+
+        if (token !== user.passwordResetToken)
+            return res.status(400).send({ error: 'Token invalid' });
+
+        const now = new Date();
+
+        if (now > user.passwordResetExpires)
+            return res.status(400).send({ error: 'Token expired, generate a new one' });
+
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+
+    } catch (err) {
+        res.statu(400).send({ error: 'Cannot reset password, try again' });
     }
 });
 
